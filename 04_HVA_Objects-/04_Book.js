eac83f3f9db57736f0155ab2c579707ec06b1b58
tha-deepakrajput <deepak23@navgurukul.org>
2024-10-04 18:26:38 +0530

    books task done

--- 04_HVA_Objects-/04_Book.js
+++ 04_HVA_Objects-/04_Book.js
@@ -1,24 +1,42 @@
 class book {
-  constructor(title, author, publisher, year, genre) {
+  constructor(title, author, year, publisher, genre) {
     this.title = title 
     this.author = author
-    this.publisher = year
+    this.year = year
+    this.publisher = publisher
     this.genre = genre
     this.classicBook = {
       title : title,
       author : author,
+      year: year,
       publisher : publisher,
       genre : genre
+    }
+    this.sciFiBook = {
+      title: title,
+      author: author,
+      year: year,
+      publisher: publisher,
+      genre: genre
     }
   }
 
   describe() {
-    console.log(`
-      ${this.name} - ${this.author} (${this.year})
-      `);
+    console.log(`${this.title} - ${this.author} (${this.year})`);
   }
 
   displayGenre() {
     console.log(`Genre : ${this.genre}`);
   }
 }
+
+// console.log(this.classicBook);
+
+let book1 = new book("Pride and Prejudice", "Jane Austen", 1813, "T. Egerton", "Classic")
+let book2 = new book("Dune", "Frank Herbert", "Chilton Books", 1965, "Science Fiction")
+
+book1.describe()
+book1.displayGenre()
+
+book2.describe()
+book2.displayGenre()
