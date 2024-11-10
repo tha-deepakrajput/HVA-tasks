c106f25bf21aacf6d242603c83eb669e7bda4000
tha-deepakrajput <deepak23@navgurukul.org>
2024-11-10 20:25:25 +0530

    updated

--- 09_Fetch_API/01_Fetch_post.js
+++ 09_Fetch_API/01_Fetch_post.js
@@ -1,34 +1,26 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 const posts = document.querySelector('#posts');
 
-// const getPosts = async () => {
-//     let response = await fetch(url);
-//     console.log(response);
-//     let data = await response.json();
-//     posts.innerText = data;
-// }
+async function getPosts() {
+    let response = await fetch(url);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+}
+
+getPosts();
+
 
 // async function getPosts() {
 //     let response = await fetch(url);
 //     console.log(response);
 //     let data = await response.json();
-//     posts.innerText = data;
+
+//     // Check if data is an array and display posts
+//     if (Array.isArray(data)) {
+//         posts.innerHTML = data.map(post => `<p><strong>${post.title}</strong>: ${post.body}</p>`).join("");
+//     } else {
+//         posts.innerText = "No posts available";
+//     }
 // }
-
 // getPosts();
-
-
-async function getPosts() {
-    let response = await fetch(url);
-    console.log(response);
-    let data = await response.json();
-
-    // Check if data is an array and display posts
-    if (Array.isArray(data)) {
-        posts.innerHTML = data.map(post => `<p><strong>${post.title}</strong>: ${post.body}</p>`).join("");
-    } else {
-        posts.innerText = "No posts available";
-    }
-}
-
-getPosts();
